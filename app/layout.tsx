1c03fbf8508a6db5aa652dec4359f6c278b250e9
Ariunzaya Batbold <ab5853@columbia.edu>
2026-02-26 15:44:45 -0500

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,19 +13,16 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
+        url: '/favicon-16x16.png',
       },
       {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
+        url: '/favicon-32x32.png',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/favicon.ico',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/apple-touch-icon.png',
   },
 }
 
